64e29c4d2f0201aa8046d9ca7f42e372edc35647
Doug Kintop <dkintop@hotmail.com>
2019-09-24 18:43:59 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,7 +1,15 @@
 class Patient 
   attr_reader :name
+  
+  @@all = []
   def initialize(name)
     @name = name 
+    @@all << self 
+  end 
+  
+  
+  def self.all 
+    
   end 
   
   def appointments
