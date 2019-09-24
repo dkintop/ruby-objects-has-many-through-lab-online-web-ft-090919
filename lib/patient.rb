79b524304209a8a04f262df4c1a66f7404bb3f58
Doug Kintop <dkintop@hotmail.com>
2019-09-24 18:48:40 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -33,3 +33,31 @@ end
 
 =end
 
+class Patient
+  
+  attr_accessor :name
+  
+  @@all = []
+  
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  
+  def new_appointment(doctor, date)
+    Appointment.new(doctor, self, date)
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def appointments
+    Appointment.all.select {|appt| appt.patient == self}
+  end
+  
+  def doctors
+    appointments.map {|appt| appt.doctor}
+  end
+  
+end
